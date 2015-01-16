2abf0038574807a6cd59046c81f007f684142d59
zhili <zhili@wzl>
2015-01-16 13:44:51 +0800

    改进抽象类

--- src/com/bit/criminalintent/CrimeActivity.java
+++ src/com/bit/criminalintent/CrimeActivity.java
@@ -7,22 +7,15 @@ import android.app.FragmentManager;
 import android.os.Bundle;
 
 
-public class CrimeActivity extends Activity {
+public class CrimeActivity extends SingleFragmentActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-        
-        FragmentManager fm =getFragmentManager(); //获得管理器
-        Fragment fragment=fm.findFragmentById(R.id.fragmentContainer);//绑定到对应的容器中
-        
-        if(fragment==null){//一个fragment事务
-        	fragment=new CrimeFragment();
-        	fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
-        }
-    }
+	@Override
+	protected Fragment createFragment() {		
+		return new CrimeFragment();
+	}
 
+ 
+   
 
 
 
